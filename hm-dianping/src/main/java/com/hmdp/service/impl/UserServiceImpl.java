dcf51c4e53e665ae807bbfc0839dbb8330787027
jiangqilei <2684442955@qq.com>
2023-06-12 11:46:12 +0800

    由session登入改成redis

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
@@ -10,10 +12,17 @@ import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
@@ -28,6 +37,8 @@ import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 //ServiceImpl<UserMapper, User>标准了mapper和实体类
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+	@Resource
+	private StringRedisTemplate stringRedisTemplate;
 	@Override
 	public Result sendCode(String phone, HttpSession session) {
 		//1.校验手机号，检验三方满足正则表达式
@@ -37,8 +48,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 		}
 		//3.符和，则生成验证码,用随机生成器andomUtil随机生成
 		String code =RandomUtil.randomNumbers(6);
-		//4.保存验证码到session
-		session.setAttribute("code",code);
+		//4.保存验证码到redis
+		stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY+phone,code,LOGIN_CODE_TTL, TimeUnit.MINUTES);
 		//5.发送验证码
 	    System.out.println("验证码："+code);
 		//6.返回ok
@@ -53,8 +64,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 			//2.如果不符合，返回错误信息
 			return Result.fail("手机号格式错误");
 		}
-		//2.校验验证码,从session中提取出刚刚获取的验证码
-		var cachecode = session.getAttribute("code");
+		//3.校验验证码,从redis中提取出刚刚获取的验证码
+		String cachecode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY+phone);
 		String code =loginForm.getCode();
 		//验证码可能过期了，所以要进行判空操作
 		if (cachecode==null||!cachecode.toString().equals(code)){
@@ -69,9 +80,21 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 			user = craeteUserWithPhone(phone);
 			
 		}
-		//7.保存用户信息到session
-		session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
-		return Result.ok();
+		//7.保存用户信息到redis
+		//7.1生成一个token，作为登录令牌
+		String token=UUID.randomUUID().toString(true);
+		//7.2将User对象转为hash存储
+		UserDTO userDTO=BeanUtil.copyProperties(user,UserDTO.class);
+		Map<String,Object>userMap=BeanUtil.beanToMap(userDTO,new HashMap<>(),
+				CopyOptions.create()
+						.setIgnoreNullValue(true)
+						.setFieldValueEditor((fieldName,fieldValue) -> fieldValue.toString()));
+		//7.3存储
+		String tokenKey=LOGIN_USER_KEY+token;
+		stringRedisTemplate.opsForHash().putAll(LOGIN_USER_KEY+tokenKey,userMap);
+		//7.4设置有效期
+		stringRedisTemplate.expire(tokenKey,LOGIN_USER_TTL,TimeUnit.MINUTES);
+		return Result.ok(token);
 	}
 
 	private User craeteUserWithPhone(String phone) {
--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,5 +1,6 @@
 package com.hmdp;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
