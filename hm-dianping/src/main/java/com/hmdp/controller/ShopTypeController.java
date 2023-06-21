b2ede68cd2c2b0ce13bbcad9f57399705c7f2561
jiangqilei <2684442955@qq.com>
2023-06-21 17:58:33 +0800

    增加了缓存穿透和击穿

--- hm-dianping/src/main/java/com/hmdp/controller/ShopTypeController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -1,6 +1,5 @@
 package com.hmdp.controller;
 
-
 import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.service.IShopTypeService;
@@ -11,24 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
 @RestController
 @RequestMapping("/shop-type")
 public class ShopTypeController {
-    @Resource
-    private IShopTypeService typeService;
+	@Resource
+	private IShopTypeService typeService;
 
-    @GetMapping("list")
-    public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
-        return Result.ok(typeList);
-    }
+	@GetMapping("list")
+	public Result queryTypeList() {
+		List<ShopType> typeList = typeService
+				.query().orderByAsc("sort").list();
+		return Result.ok(typeList);
+	}
 }
