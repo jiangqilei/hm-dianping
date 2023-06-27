8c4039b3e870d4f359036e57090e94937d6fff16
jiangqilei <2684442955@qq.com>
2023-06-27 17:59:16 +0800

    一人一单

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -47,7 +47,15 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 		if (voucher.getStock()<1){
 			return Result.fail("库存不足");
 		}
-		//5.扣减库存
+		// 5.一人一单
+		Long userId = UserHolder.getUser().getId();
+		//5.1.查询订单
+		int count = query().eq("user_id",userId).eq("voucher_id",voucherId).count();
+		//5.2判断是否存在
+		if (count>0){
+			return Result.fail("用户已经购买过了");
+		}
+		//6.扣减库存
 		boolean success= seckillVoucherService.update()
 				.setSql("stock=stock-1")
 				.eq("voucher_id",voucherId)
@@ -56,13 +64,13 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 		if(!success){
 			return Result.fail("库存不足");
 		}
-		//6.创建订单
+		//7.创建订单
          VoucherOrder voucherOrder=new VoucherOrder();
-		//6.1.订单id
+		//7.1.订单id
 		long orderid = redisIdWorker.nextId("order");
 		voucherOrder.setId(orderid);
-		//6.2.用户id
-        Long userId= UserHolder.getUser().getId();
+		//7.2.用户id
+        //Long userId= UserHolder.getUser().getId();
 		voucherOrder.setUserId(userId);
 		//6.3.代金券id
         voucherOrder.setVoucherId(voucherId);
